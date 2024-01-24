12c86106f4d43107f37ff2db86b538567f52574c
João Vieira <netojoao599@gmail.com>
2024-01-23 23:23:53 -0300

    Slug automatico e cadastro de produtos

--- produto/models.py
+++ produto/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.conf import settings
 from PIL import Image
 import os
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
-    descricao_curta = models.TextField(max_length=255)
+    descricao_curta = models.TextField(max_length=255, verbose_name='Descrição Curta')
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produto_imagens/%Y/%m', blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField(default=0)
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(default=0, verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo')
     tipo = models.CharField(
         default='V',
         max_length=1,
@@ -21,6 +22,14 @@ class Produto(models.Model):
             ('S', 'Simples'),
         )
     )
+
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_promocional_formatado.short_description = 'Preço'
 
     def __str__(self):
         return self.nome
@@ -42,6 +51,10 @@ class Produto(models.Model):
         img_pil.close()
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         img_max_width = 800
