af3d55640bb38490e608ab753b1cdda5faaa6781
João Vieira <netojoao599@gmail.com>
2024-01-30 14:10:43 -0300

    Templates - listando produtos

--- loja/settings.py
+++ loja/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join('templates/static')
+    os.path.join('produto/templates/static')
 ]
 
 MEDIA_URL = '/media/'
