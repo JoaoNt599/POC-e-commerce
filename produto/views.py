1701cb23e1c2c12ccc07301a2cd1e6484c9873ad
João Vieira <netojoao599@gmail.com>
2024-01-31 02:13:18 -0300

    Quantidade no carrinho e verificando estoque

--- produto/views.py
+++ produto/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from . import models
 from django.contrib import messages
 
+from pprint import pprint
+
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -37,6 +39,29 @@ class AdicionarAoCarrinho(View):
         
         
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
+
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        variacao_id = variacao.id
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ''
+
+        if variacao.estoque < 1:
+            messages.error(
+                self.request,
+                'Produto fora do estoque'
+            )
+            return redirect(http_referer)
 
         if not self.request.session.get('carrinho'):
             self.request.session['carrinho'] = {}
@@ -47,12 +72,36 @@ class AdicionarAoCarrinho(View):
 
         if variacao_id in carrinho:
             # TODO: Variação encontrada no carrinho
-            pass
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1 
+
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request,
+                    f'Estoque insuficiente para {quantidade_carrinho}x no '
+                    f'produto "{produto_nome}". Foi adicionado {variacao_estoque} x '
+                    f'ao seu carrinho.'
+                )
+                quantidade_carrinho = variacao_estoque
+            
+            carrinho[variacao_id]['quantidade'] = quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario * quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional * quantidade_carrinho
         else:
             # TODO: Variação não encontrada no carrinho
-            pass
+            carrinho[variacao_id] = {
+                'produto_id': produto_id,
+                'produto_nome': produto_nome,
+                'variacao_nome': variacao_nome,
+                'variacao_id': variacao_id,
+                'preco_unitario': preco_unitario, 
+                'preco_unitario_promocional': preco_unitario_promocional,
+                'slug': 1,
+                'imagem': imagem
+            }
 
-        
+        self.request.session.save()
+        pprint(carrinho)
         return HttpResponse('Adicionar Ao Carrinho')
 
 
