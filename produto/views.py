b5a1d6f0fc7b00449abc9c48315947b1f697fae1
João Vieira <netojoao599@gmail.com>
2024-01-31 01:12:40 -0300

    Verificando variação id 'vid'

--- produto/views.py
+++ produto/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
+from django.contrib import messages
 
 
 class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
-    paginate_by = 3
+    paginate_by = 6
 
 
 class DetalheProduto(DetailView):
@@ -22,6 +23,36 @@ class DetalheProduto(DetailView):
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
+
+         # TODO: Verificando variacao id 'vid'
+        http_referer = self.request.META.get('HTTP_REFERER', reverse('produto:lista'))
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto Inexistente na Base de Dados'
+            )
+            return redirect(http_referer)
+        
+        
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+        
+
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            # TODO: Variação encontrada no carrinho
+            pass
+        else:
+            # TODO: Variação não encontrada no carrinho
+            pass
+
+        
         return HttpResponse('Adicionar Ao Carrinho')
 
 
